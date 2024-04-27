c6b1d485c684b067291868ff8a1a5a05c70d03aa
Argenis De La Rosa <theonlyhennygod@gmail.com>
2024-04-27 18:55:01 -0400

    add files

---
+++ index.js
@@ -0,0 +1,57 @@
+const canvas = document.querySelector('canvas')
+
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const gravity = 0.5;
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.width = 30;
+        this.height = 30;
+    }
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+
+        // bounce effect
+        // if (this.position.y + this.height > canvas.height) {
+        //     this.velocity.y = -this.velocity.y;
+        // }
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
+    }
+}
+
+const player = new Player();
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+}
+
+animate();
+
+addEventListener('keydown', (event) => { 
+
+    console.log('keydown');
+})
